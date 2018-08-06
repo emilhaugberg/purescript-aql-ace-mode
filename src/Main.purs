99e30c2207e3106df3434a6ccd7db19a2f1d8afe
Emil Haugberg <emilveen@gmail.com>
2018-08-06 20:27:02 +0200

    Simple editor.

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,16 @@
 module Main where
 
+import Ace as Ace
+import Ace.Editor
+import Ace.EditSession
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
 main :: Effect Unit
-main = do
-  log "Hello sailor!"
+main = void $ do
+  editor  <- Ace.edit "editor" Ace.ace
+  _       <- setTheme "ace/theme/chrome" editor
+  session <- getSession editor
+
+  setMode "ace/mode/javascript" session
