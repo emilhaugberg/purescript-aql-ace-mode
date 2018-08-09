9e17df418045eb4d68a8fe88112c442d9e1b1f6d
Emil Haugberg <emilveen@gmail.com>
2018-08-09 17:30:26 +0200

    It's finally working.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,5 @@
-require('./mode-aql.js');
-require('../output/Main/index.js').main();
+var ace = require('./ace/ace');
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/ambiance");
+editor.session.setMode("ace/mode/aql");
